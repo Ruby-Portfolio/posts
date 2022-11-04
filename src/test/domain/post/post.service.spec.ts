16315f31013f667144eb822963de7515d917da96
MisterRuby <ruby8700@naver.com>
2022-11-04 23:35:22 +0900

    test : 게시글 수정 단위 테스트
    수정할 게시글이 존재하지 않을 경우 PostNotFoundException 예외 처리
    비밀번호가 일치하지 않을 경우 PasswordMismatchException 예외 처리
    게시글 수정 성공

--- src/test/domain/post/post.service.spec.ts
+++ src/test/domain/post/post.service.spec.ts
@@ -1,9 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostRepository } from '../../../domain/post/post.repository';
 import { PostService } from '../../../domain/post/post.service';
-import { AddPostDto } from '../../../domain/post/post.request.dto';
-import { InsertResult } from 'typeorm';
-import { PostNotFoundException } from '../../../domain/post/post.exception';
+import {
+  AddPostDto,
+  UpdatePostDto,
+} from '../../../domain/post/post.request.dto';
+import { InsertResult, UpdateResult } from 'typeorm';
+import {
+  PasswordMismatchException,
+  PostNotFoundException,
+} from '../../../domain/post/post.exception';
 import { Post } from '../../../domain/post/post.entity';
 
 describe('PostService', () => {
@@ -63,6 +69,75 @@ describe('PostService', () => {
         .mockResolvedValue(Promise.resolve(post));
 
       return expect(postService.getPost(10)).resolves.toEqual(post);
+    });
+  });
+
+  describe('게시글 수정', () => {
+    describe('게시글 수정 실패', () => {
+      test('수정할 게시글이 존재하지 않을 경우 PostNotFoundException 예외 처리', async () => {
+        const post: Post = {
+          id: 10,
+          title: '테스트',
+          password: 'asd13ad',
+          content: 'asdasd',
+          author: '루비',
+        } as Post;
+
+        jest
+          .spyOn(postRepository, 'findOneBy')
+          .mockResolvedValue(Promise.resolve(null));
+
+        return expect(postService.updatePost(10, post)).rejects.toThrowError(
+          new PostNotFoundException(),
+        );
+      });
+
+      test('비밀번호가 일치하지 않을 경우 PasswordMismatchException 예외 처리', async () => {
+        const post = new Post();
+
+        const updatePost: UpdatePostDto = {
+          title: '테스트',
+          password: 'asdasd1123',
+          content: 'asdasd',
+          author: '루비',
+        } as Post;
+
+        jest
+          .spyOn(postRepository, 'findOneBy')
+          .mockResolvedValue(Promise.resolve(post));
+        jest
+          .spyOn(post, 'equalsPassword')
+          .mockResolvedValue(Promise.resolve(false));
+
+        return expect(
+          postService.updatePost(10, updatePost),
+        ).rejects.toThrowError(new PasswordMismatchException());
+      });
+    });
+
+    test('게시글 수정 성공', async () => {
+      const post = new Post();
+
+      const updatePost: UpdatePostDto = {
+        title: '테스트',
+        password: 'asdasd1123',
+        content: 'asdasd',
+        author: '루비',
+      } as Post;
+
+      jest
+        .spyOn(postRepository, 'findOneBy')
+        .mockResolvedValue(Promise.resolve(post));
+      jest
+        .spyOn(post, 'equalsPassword')
+        .mockResolvedValue(Promise.resolve(true));
+      jest
+        .spyOn(postRepository, 'update')
+        .mockResolvedValue(new UpdateResult());
+
+      return expect(postService.updatePost(10, updatePost)).resolves.toEqual(
+        new UpdateResult(),
+      );
     });
   });
 });
