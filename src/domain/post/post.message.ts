366cf5d7b0c26bd2dd9e6784f490a809938a4e17
MisterRuby <ruby8700@naver.com>
2022-11-04 10:19:37 +0900

    update : Post 엔티티 Validation 적용

---
+++ src/domain/post/post.message.ts
@@ -0,0 +1,8 @@
+export enum PostMessage {
+  AUTHOR_NOT_EMPTY = '작성자 명은 최소 1글자 이상이어야 합니다.',
+  PASSWORD = '비밀번호는 최소 6글자 이상이어야 합니다.',
+  TITLE_MIN_LENGTH = '제목은 최소 2글자 이상이어야 합니다.',
+  TITLE_MAX_LENGTH = '제목은 최대 20 글자를 초과할 수 없습니다.',
+  CONTENT_MIN_LENGTH = '글 내용은 최소 2글자 이상이어야 합니다.',
+  CONTENT_MAX_LENGTH = '글 내용은 최대 200 글자를 초과할 수 없습니다.',
+}
