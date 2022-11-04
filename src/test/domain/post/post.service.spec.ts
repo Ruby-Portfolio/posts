201263f32066d774f3fb9ed0a4db4846bd044813
MisterRuby <ruby8700@naver.com>
2022-11-04 21:20:27 +0900

    feat : 게시글 등록 단위 테스트
    게시글 상세 조회 결과가 없을 경우 PostNotFoundException 예외 처리
    게시글 상세 조회 성공

--- src/test/domain/post/post.service.spec.ts
+++ src/test/domain/post/post.service.spec.ts
@@ -3,6 +3,8 @@ import { PostRepository } from '../../../domain/post/post.repository';
 import { PostService } from '../../../domain/post/post.service';
 import { AddPostDto } from '../../../domain/post/post.request.dto';
 import { InsertResult } from 'typeorm';
+import { PostNotFoundException } from '../../../domain/post/post.exception';
+import { Post } from '../../../domain/post/post.entity';
 
 describe('PostService', () => {
   let postRepository: PostRepository;
@@ -25,12 +27,42 @@ describe('PostService', () => {
         title: '게시글 등록 테스트',
         content: '게시글 등록 테스트 본문',
       };
-
+      const insertResult = new InsertResult();
       jest
         .spyOn(postRepository, 'insert')
-        .mockResolvedValue(Promise.resolve(new InsertResult()));
+        .mockResolvedValue(Promise.resolve(insertResult));
 
-      expect(await postService.addPost(addPost)).toEqual(new InsertResult());
+      return expect(postService.addPost(addPost)).resolves.toEqual(
+        insertResult,
+      );
+    });
+  });
+
+  describe('게시글 상세 조회', () => {
+    test('게시글 상세 조회 결과가 없을 경우 PostNotFoundException 예외 처리', async () => {
+      jest
+        .spyOn(postRepository, 'findOneBy')
+        .mockResolvedValue(Promise.resolve(null));
+
+      return expect(postService.getPost(10)).rejects.toThrowError(
+        new PostNotFoundException(),
+      );
+    });
+
+    test('게시글 상세 조회 성공', async () => {
+      const post: Post = {
+        id: 1,
+        title: '테스트',
+        password: 'asd13ad',
+        content: 'asdasd',
+        author: '루비',
+      } as Post;
+
+      jest
+        .spyOn(postRepository, 'findOneBy')
+        .mockResolvedValue(Promise.resolve(post));
+
+      return expect(postService.getPost(10)).resolves.toEqual(post);
     });
   });
 });
