328365bcecac3fc379079a4b9fe337975a4d268d
MisterRuby <ruby8700@naver.com>
2022-11-04 23:33:00 +0900

    update : 게시글 수정 dto의 비밀번호 validation 변경
    기존 숫자 포함 검증을 제거하여 임의의 사용자가 비밀번호의 틀린 원인을 알지 못하게 하기 위함

--- src/domain/post/post.request.dto.ts
+++ src/domain/post/post.request.dto.ts
@@ -1,7 +1,8 @@
 import { PickType } from '@nestjs/swagger';
 import { Post } from './post.entity';
 import { IsOptionalId } from '../../common/validation/IsOptionalId.validation';
-import { IsOptional } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
+import { PostErrorMessage } from './post.error.message';
 
 export class AddPostDto extends PickType(Post, [
   'author',
@@ -20,7 +21,9 @@ export class GetPostsDto {
 
 export class UpdatePostDto extends PickType(Post, [
   'author',
-  'password',
   'title',
   'content',
-] as const) {}
+] as const) {
+  @IsString({ message: PostErrorMessage.PASSWORD })
+  password: string;
+}
